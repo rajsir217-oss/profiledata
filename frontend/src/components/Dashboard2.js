24553882b5351aa15b832646a6a28d9299ea0d58
rajsir <rajsir217@gmail.com>
2026-04-20 12:28:19 -0700

    changes made on 2026-04-20 | 12:28:19

--- frontend/src/components/Dashboard2.js
+++ frontend/src/components/Dashboard2.js
@@ -2098,7 +2098,10 @@ const Dashboard2 = () => {
         >
           <div className="activity-group-title">
             <span className="activity-group-icon">🔐</span>
-            <h2>Data Requests</h2>
+            <h2>
+              <span className="label-desktop">Contact &amp; Photo Requests</span>
+              <span className="label-mobile">Data Requests</span>
+            </h2>
             <span className="activity-group-count">
               {dashboardData.incomingContactRequests.length + dashboardData.myRequests.length}
             </span>
